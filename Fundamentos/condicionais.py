e38d19a19b0b04e7472aac0fba4619d2a8427b46
Robson <robson14br@outlook.com>
2024-06-24 22:14:23 -0300

    Estudos

---
+++ Fundamentos/condicionais.py
@@ -0,0 +1,9 @@
+MAIOR_Idade = 18
+
+idade = int(input('informe sua idade: '))
+
+if idade >= MAIOR_Idade:
+    print('Maior idade, pode tirar carteira')
+
+if idade < MAIOR_Idade:
+    print('Voce ainda nao pode tirar carteira')
